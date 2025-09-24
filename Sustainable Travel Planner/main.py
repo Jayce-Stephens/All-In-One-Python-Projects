bd93b7b53ce69c17c6fab1e19e38bc66a7478868
Jayce <jstephe3@xula.edu>
2025-09-23 21:12:59 -0500

    Create distance function

--- Sustainable Travel Planner/main.py
+++ Sustainable Travel Planner/main.py
@@ -1,3 +1,6 @@
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic #install geopy in a virtual environment
+
 
 def main():
     print("Let's plan your eco-friendly trip!")
@@ -5,10 +8,20 @@ def main():
     destination = input("Enter your travel destination: ")
     travel_mode = input("Enter your preferred mode of travel (e.g., train, bus, bike): ")
     accommodation_type = input("Enter your preferred accommodation type (e.g., eco-lodge, hostel, hotel): ")
+
+    print(distance(current_location, destination))
     eco_activities =""
 
 
+def distance(current_location, destination):
+    
+    geolocator = Nominatim(user_agent="sustainable_travel_planner")
+    loc1 = geolocator.geocode(current_location)
+    loc2 = geolocator.geocode(destination)
 
+    coor1 = (loc1.latitude, loc1.longitude)
+    coor2 = (loc2.latitude, loc2.longitude)
+    return ("Distance: ", geodesic(coor1, coor2).km, "km") #function of geopy that returns the distance between two coordinates
 
 
 if __name__ == "__main__":

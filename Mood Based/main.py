bf54476ef33ece1a4ab4b2c6354765832011a5d0
Jayce <jstephe3@xula.edu>
2025-09-24 01:26:29 -0500

    Create function to generate playlist based off of the given mood

--- Mood Based/main.py
+++ Mood Based/main.py
@@ -5,6 +5,19 @@ def main():
     mood = input("Enter your current mood (happy, sad, energetic, calm): ").lower()
     print(f"Here is a playlist for your {mood} mood:")
 
+    playlist = generate_playlist(mood)
+    for i, song in enumerate(playlist):
+        print(f"{i + 1}: {song}")
+
+def generate_playlist(mood):
+    playlists = {
+        "happy": ["Chicago - Micheal Jackson", "There Was This Girl - Riley Green", "The Days - CHRYSTAL & NOTION"],
+        "sad": ["Pearls - Sade", "Distant Lover - Marvin Gaye", "Perfect Circle - Mac Miller"],
+        "energetic": ["loud room - Lumisoun", "FOREGROUND - Lonown", "My Address Public - NBA YoungBoy"],
+        "calm": ["Juna - Clairo", "Over and Over - The Altons", "Not You Too(feat. Chris Brown) - Drake"]
+    }
+    return playlists.get(mood, ["No playlist available for this mood. Try happy, sad, energetic, or calm."])
+
 
 
 if __name__ == "__main__":
